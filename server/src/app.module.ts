05618a92c47cf6a842407cbe2a1b7f7cce64d8f6
Andrei <andrei.vupt@gmail.com>
2025-08-16 20:23:44 -0300

    feat:config deploy

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,16 +6,30 @@ import { WordsModule } from './words/words.module';
 import { DictionaryModule } from './dictionary/dictionary.module';
 import { AppController } from './app.controller';
 import { RedisModule } from './redis/redis.module';
+import { HealthController } from './health.controller';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => ({
+        store: await redisStore({
+          url: process.env.REDIS_URL, 
+        }),
+      }),
+    }),
     AuthModule, 
     UsersModule,
     WordsModule,
     DictionaryModule,
     RedisModule
   ],
-  controllers: [AppController],
+  controllers: [
+    AppController,
+    HealthController
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
