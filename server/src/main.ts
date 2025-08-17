1ef0ba4fbc8c44572873af14d70c962a095f493b
Andrei <andrei.vupt@gmail.com>
2025-08-17 12:48:22 -0300

    fix: update enviroments

--- server/src/main.ts
+++ server/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     ],
     credentials: true,
     methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-    allowedHeaders: ['Content-Type','Authorization','X-Requested-With'],
+    allowedHeaders: ['Content-Type','Authorization','X-Requested-With','Accept','Origin'],
     optionsSuccessStatus: 204,
   });
 
